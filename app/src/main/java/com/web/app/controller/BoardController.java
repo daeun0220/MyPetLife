6252bf62b0728648eeb73bda787d95931bd63d0f
daeun0220 <daeunkim1284@gmail.com>
2024-03-18 01:59:58 +0900

    [Feat] Querydsl 게시판 검색 조회

--- app/src/main/java/com/web/app/controller/BoardController.java
+++ app/src/main/java/com/web/app/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.web.app.controller;
 
 import com.web.app.dto.request.BoardRequestDto;
+import com.web.app.dto.response.BoardDto;
 import com.web.app.dto.response.BoardResponseDto;
 import com.web.app.dto.response.ResultType;
+import com.web.app.repository.BoardRepository;
 import com.web.app.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/mypetlife")
@@ -16,15 +21,22 @@ import java.util.List;
 @Slf4j
 public class BoardController {
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
 
     @PostMapping("/board/{userId}")
     public Long createBoard(@PathVariable Long userId, @RequestBody BoardRequestDto boardRequestDto) {
         return boardService.createBoard(userId, boardRequestDto);
     }
-
     @GetMapping("/board/{boardId}")
     public BoardResponseDto searchById(@PathVariable Long boardId) {
         return boardService.searchById(boardId);
+    }
+    @GetMapping("/board")
+    public Page<BoardDto> searchAll(@RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                    @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+                                    @RequestParam(required = false) String title) {
+        Pageable pageable = PageRequest.of(page, size);
+        return boardRepository.findBySearchOption(pageable, title);
     }
     @PatchMapping("/board/{boardId}")
     public ResultType updateBoard(@PathVariable Long boardId, @RequestBody BoardRequestDto boardRequestDto) {
---
+++ app/src/main/java/com/web/app/dto/response/BoardDto.java
@@ -0,0 +1,35 @@
+package com.web.app.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.web.app.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class BoardDto {
+    private String title;
+    private LocalDate createDate;
+    private LocalDate modifyDate;
+    private String writer;
+    public static BoardDto from(Board board) {
+        return BoardDto.builder()
+                .title(board.getTitle())
+                .createDate(board.getCreateDate())
+                .modifyDate(board.getModifyDate())
+                .writer(board.getWriter())
+                .build();
+    }
+    @QueryProjection
+    public BoardDto(String title, LocalDate createDate, LocalDate modifyDate, String wrtier) {
+        this.title = title;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+        this.writer = wrtier;
+    }
+}

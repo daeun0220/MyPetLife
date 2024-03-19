e55584c2d1d60aabafafc8ef0c8bd3172bfd9ef8
daeun0220 <daeunkim1284@gmail.com>
2024-03-19 22:59:48 +0900

    [Feat] #1 좋아요 게시물 조회

--- app/src/main/java/com/web/app/controller/LikeController.java
+++ app/src/main/java/com/web/app/controller/LikeController.java
@@ -1,12 +1,13 @@
 package com.web.app.controller;
 
+import com.web.app.dto.response.BoardDto;
 import com.web.app.dto.response.ResultType;
 import com.web.app.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/mypetlife")
@@ -18,5 +19,11 @@ public class LikeController {
     public ResultType setWish(@PathVariable Long boardId, @PathVariable Long userId) {
         likeService.like(boardId, userId);
         return ResultType.SUCCESS;
+    }
+    @GetMapping("/like/{userId}")
+    public List<BoardDto> searchLike(@PathVariable Long userId,
+                                     @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                     @RequestParam(name = "size", required = false, defaultValue = "10") int size) {
+        return likeService.searchLike(userId, page, size);
     }
 }
